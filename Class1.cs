33e72f425db16b36aed3845320ce7930cbbab58e
black <black@DESKTOP-Q9RCIUM>
2023-10-27 18:07:56 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace _1722
+{
+    public class Account
+    {
+        public int Id { get; private set; }
+        public int CurrencyId { get; set; }
+        public int AccountTypeId { get; set; }
+        public int FundsTypeId { get; set; }
+        public int InstitutionId { get; set; }
+        public decimal Balance { get; private set; }
+        public string Note
+        {
+            get; set;
+        }
+
+        private static int nextId = 1;
+
+        public Account()
+        {
+            Id = nextId++;
+        }
+
+        public void IncreaseBalance(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void DecreaseBalance(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+        }
+    }
+}
